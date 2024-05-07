1c71fdcbe5c04b3f75f35f40afaf0e025286e7b0
VEGERNOM <144153261+VEGERNOM@users.noreply.github.com>
2024-05-07 18:42:07 +0700

    Add files via upload

---
+++ gabut.sh
@@ -0,0 +1,59 @@
+#gabut
+
+clear
+sleep 5
+echo -e "         1. Hack Pulsa
+         2. Kill alien
+         3. Geser matahari
+         4. Geser bumi
+         5. Bunuh diri"
+echo
+read -p "PILIH:" OP
+echo
+if [ $OP == 1 ]
+then
+sleep 5
+echo "sabar lagi proses"
+sleep 5
+echo
+echo "modal lah ya kali nge hack"
+fi
+
+if [ $OP == 2 ]
+then
+sleep 5
+echo "sedang membunuh sabar"
+sleep 5
+echo
+echo "dor dor dor mati kau"
+sleep 5
+echo
+echo " victory"
+fi 
+
+if [ $OP == 3 ]
+then
+sleep 5
+echo "proses penggeseran resiko tanggung sendiri"
+sleep 5
+echo
+echo "waduh mataharinya bergeser 99999999999999999km"
+fi
+
+if [ $OP == 4 ]
+then
+sleep 5
+echo "waduh bumi lagi di geser"
+sleep 5
+echo
+echo "wah gila lu bree geser bumi sampai 99999999999999km"
+fi
+
+if [ $OP == 5 ]
+then
+sleep 5
+echo " sabar lagi di kirim malaikat izroil ke lokasi lu bree"
+sleep 5
+echo
+echo " jangan bunuh diri bree dosa"
+fi
